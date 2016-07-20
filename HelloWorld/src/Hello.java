e988486af45d6070e1a76ecf6a212cc520f078ad
Damin Wang <damin.wang@cbp.dhs.gov>
2016-07-20 07:44:00 -0400

    added printing date time

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,8 +1,13 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		Calendar cal = Calendar.getInstance();
+		DateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:SS");
+		System.out.println("Hello World!" + df.format(cal.getTime()));
 	}
 
 }
